9ae6f684698152608225298cf511392499bf7b8b
Michael Isasi <Jetmax25@gmail.com>
2018-03-12 22:01:02 -0400

    Added creation storyboard

---
+++ LazyA/LazyA/View Controllers/CreateCourseViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CreateCourseViewController.swift
+//  LazyA
+//
+//  Created by Michael Isasi on 3/12/18.
+//  Copyright © 2018 Jetmax25. All rights reserved.
+//
+
+import UIKit
+
+class CreateCourseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
